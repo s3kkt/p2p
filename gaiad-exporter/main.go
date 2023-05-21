d0fbbf108ae532ac4b38c9c3f0ed56894049810c
sergey.ryabov <sergey.ryabov@city-mobil.ru>
2023-05-21 07:40:47 +0300

    add some exporter logging

--- gaiad-exporter/main.go
+++ gaiad-exporter/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"log"
 	"math"
 	"net/http"
 	"os/exec"
@@ -87,8 +88,8 @@ func ParseFlags() (string, string, string) {
 func GetGaiaStatus() []byte {
 	//JSONPayload := `{"NodeInfo":{"protocol_version":{"p2p":"8","block":"11","app":"0"},"id":"6f4d5f7ecb3d73ca291658d5d2368e23c34d9122","listen_addr":"tcp://0.0.0.0:26656","network":"theta-testnet-001","version":"0.34.27","channels":"40202122233038606100","moniker":"sr","other":{"tx_index":"on","rpc_address":"tcp://127.0.0.1:26657"}},"SyncInfo":{"latest_block_hash":"527D77A25A007353170611E0A3FB7163A281A47EF31DB9A46AAF25D42A5E1515","latest_app_hash":"48C6486DA403F98F13C22D7FF0114899CA88BFE2AC734B2A6C5E0C172046C8B6","latest_block_height":"16041033","latest_block_time":"2023-05-19T16:36:07.825229312Z","earliest_block_hash":"0C78904CC0CF2450C7A0845425FA2CA07A1003671B3DCD4CBAB9D160FFE56948","earliest_app_hash":"337522801C1F4279E170123DFA3D0DA604307F5F2D501DFE9B90507212F78D8E","earliest_block_height":"16040001","earliest_block_time":"2023-05-19T15:01:26.554070159Z","catching_up":true},"ValidatorInfo":{"Address":"AA20C839B1BBEF4BE431829A658C2EA6B28D1D22","PubKey":{"type":"tendermint/PubKeyEd25519","value":"gl8QDOmLJswTht/ID8IeTeXtYghwNUeysOglrSbjPEY="},"VotingPower":"0"}}`
 
-	//service := gaiadPath
 	argument := "status"
+	log.Printf("Executing %s %s", gaiadPath, argument)
 	command, _ := exec.Command(gaiadPath, argument).Output()
 	return command
 }
@@ -96,7 +97,11 @@ func GetGaiaStatus() []byte {
 func handleMetrics() {
 	var gaiaStatus GaiaStatus
 	for {
-		json.Unmarshal(GetGaiaStatus(), &gaiaStatus)
+		err := json.Unmarshal(GetGaiaStatus(), &gaiaStatus)
+		if err != nil {
+			log.Printf("Cannot unmarshal status JSON. Reason: %v", err)
+			return
+		}
 
 		if bn, err := strconv.ParseFloat(gaiaStatus.NodeInfo.ProtocolVersion.Block, 64); err == nil {
 			blockNumber.Set(bn)
@@ -131,6 +136,13 @@ func metricsServe(listenAddress, metricsPath string) error {
 }
 
 func main() {
+	log.Printf("Starting exporter on :%s%s for %s", listenPort, metricsPath, gaiadPath)
+
 	go handleMetrics()
-	metricsServe(":"+listenPort, metricsPath)
+
+	err := metricsServe(":"+listenPort, metricsPath)
+	if err != nil {
+		log.Printf("Cannot serve metrics. Reason: %v", err)
+		return
+	}
 }
