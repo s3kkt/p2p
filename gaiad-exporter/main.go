d1e2829fe6b1ed9a94d81cbc5bb5673059ae9e23
sergey.ryabov <sergey.ryabov@city-mobil.ru>
2023-05-21 08:12:28 +0300

    add some debug logging

--- gaiad-exporter/main.go
+++ gaiad-exporter/main.go
@@ -91,7 +91,7 @@ func GetGaiaStatus() []byte {
 	argument := "status"
 	log.Printf("Executing %s %s", gaiadPath, argument)
 	command, _ := exec.Command(gaiadPath, argument).Output()
-	log.Printf("Status JSON is: %v", string(command))
+	log.Printf("DEBUG: status JSON is: %v", command)
 	return command
 }
 
